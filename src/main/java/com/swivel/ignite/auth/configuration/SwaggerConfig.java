3cd841e8fbd7c1b9216378fc6ca0b8d776a9042f
Nawaz-swivel <nawas@swivelgroup.com.au>
2023-03-31 13:54:14 +0530

    test

--- src/main/java/com/swivel/ignite/auth/configuration/SwaggerConfig.java
+++ src/main/java/com/swivel/ignite/auth/configuration/SwaggerConfig.java
@@ -1,29 +1,40 @@
 package com.swivel.ignite.auth.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.Parameter;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static com.google.common.collect.Lists.*;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    @Value("${security.oauth2.client.client-id}")
+    private String clientId;
+    @Value("${security.oauth2.client.client-secret}")
+    private String clientSecret;
+    @Value("${security.oauth2.link}")
+    private String authLink;
+
 
     @Bean
     public Docket api() {
-        //Adding Header
         ParameterBuilder aParameterBuilder = new ParameterBuilder();
         List<Parameter> aParameters = new ArrayList<Parameter>();
 
@@ -36,22 +47,58 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(generateAPIInfo())
                 .select()
-                //Here adding base package to scan controllers. This will scan only controllers inside
-                //specific package and include in the swagger documentation
-                .apis(RequestHandlerSelectors.basePackage("com.swivel.ignite.auth"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-//                .ignoredParameterTypes(HeaderVo.class)
+                .securitySchemes(Collections.singletonList(securitySchema()))
+                .securityContexts(Collections.singletonList(securityContext()))
+                .pathMapping("/")
                 .globalOperationParameters(aParameters);
     }
 
-    //Api information
-    private ApiInfo generateAPIInfo() {
-        return new ApiInfo("Ignite Authorization Service", "Implementing Swagger with SpringBoot Application", "1.0.0",
-                "", getContacts(), "", "", new ArrayList<>());
+    private OAuth securitySchema() {
+        List<AuthorizationScope> authorizationScopeList = newArrayList();
+        authorizationScopeList.add(new AuthorizationScope("read", "read all"));
+        authorizationScopeList.add(new AuthorizationScope("trust", "trust all"));
+        authorizationScopeList.add(new AuthorizationScope("write", "access all"));
+
+        List<GrantType> grantTypes = newArrayList();
+        GrantType creGrant = new ResourceOwnerPasswordCredentialsGrant(authLink + "/oauth/token");
+
+        grantTypes.add(creGrant);
+
+        return new OAuth("oauth2schema", authorizationScopeList, grantTypes);
     }
 
-    // Developer Contacts
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth())
+                .forPaths(PathSelectors.ant("/user/**")).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+
+        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[3];
+        authorizationScopes[0] = new AuthorizationScope("read", "read all");
+        authorizationScopes[1] = new AuthorizationScope("trust", "trust all");
+        authorizationScopes[2] = new AuthorizationScope("write", "write all");
+
+        return Collections.singletonList(new SecurityReference("oauth2schema", authorizationScopes));
+    }
+
+    @Bean
+    public SecurityConfiguration securityInfo() {
+        return new SecurityConfiguration(clientId, clientSecret, "", "", "",
+                ApiKeyVehicle.HEADER, "", " ");
+    }
+
+
+    private ApiInfo generateAPIInfo() {
+        return new ApiInfo("Ignite Authorization Service",
+                "Implementing Swagger with SpringBoot Application", "1.0.0", "",
+                getContacts(), "", "", new ArrayList<>());
+    }
+
+
     private Contact getContacts() {
         return new Contact("Mohamed Nawaz", "", "nawas@swivelgroup.com.au");
     }
